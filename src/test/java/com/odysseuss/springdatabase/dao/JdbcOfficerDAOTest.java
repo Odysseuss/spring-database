2d697041a054faf35b384c98590914f2b2c17f2f
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-01 16:06:55 -0400

    Update JdbcOfficerDAOTest.java

--- src/test/java/com/odysseuss/springdatabase/dao/JdbcOfficerDAOTest.java
+++ src/test/java/com/odysseuss/springdatabase/dao/JdbcOfficerDAOTest.java
@@ -1,10 +1,16 @@
 package com.odysseuss.springdatabase.dao;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootTest
 public class JdbcOfficerDAOTest {
 
-    
+    @Autowired
+    JdbcOfficerDAO dao;
+
     @Test
     void testCount() {
 
