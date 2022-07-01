bdbe5dd1365b0e80ecbac2959c9ae2335cf01684
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-01 16:13:05 -0400

    Update JdbcOfficerDAOTest.java

--- src/test/java/com/odysseuss/springdatabase/dao/JdbcOfficerDAOTest.java
+++ src/test/java/com/odysseuss/springdatabase/dao/JdbcOfficerDAOTest.java
@@ -1,8 +1,17 @@
 package com.odysseuss.springdatabase.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.odysseuss.springdatabase.entities.Officer;
+import com.odysseuss.springdatabase.entities.Rank;
 
 
 @SpringBootTest
@@ -32,12 +41,25 @@ public class JdbcOfficerDAOTest {
     }
 
     @Test
-    void testFindById() {
+    void testFindByIdThatExists() {
 
+        Optional<Officer> officer = dao.findById(1);
+        assertTrue(officer.isPresent());
+        assertEquals(1, officer.get().getId());
+    }
+
+    @Test
+    void testFindByIdThatDoesNotExist() {
+        Optional<Officer> officer = dao.findById(999);
+        assertTrue(officer.isEmpty());
     }
 
     @Test
     void testSave() {
+
+        Officer officer = new Officer(Rank.ADMIRAL, "Akbar", "Fishman");
+        officer = dao.save(officer);
+        assertNotNull(officer.getId());
 
     }
 }
