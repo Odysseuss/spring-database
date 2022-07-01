b234de65bd18fdcb3f2c97e40666a2c597ebd939
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-01 14:55:00 -0400

    Add DAO interface methods

--- src/main/java/com/odysseuss/springdatabase/dao/OfficerDAO.java
+++ src/main/java/com/odysseuss/springdatabase/dao/OfficerDAO.java
@@ -1,5 +1,20 @@
 package com.odysseuss.springdatabase.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.odysseuss.springdatabase.dao.entities.Officer;
+
 public interface OfficerDAO {
+
+    Officer save(Officer officer);
+
+    Optional<Officer> findById(Integer id);
+
+    List<Officer> findAll();
+
+    long count();
+    void delete(Officer officer);
+    boolean existsById(Integer id);
     
 }
