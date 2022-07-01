7fc507666265fdef8f9043adc9a237d814fb6991
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-01 15:05:40 -0400

    Adding template

--- src/main/java/com/odysseuss/springdatabase/dao/JdbcOfficerDAO.java
+++ src/main/java/com/odysseuss/springdatabase/dao/JdbcOfficerDAO.java
@@ -3,6 +3,8 @@ package com.odysseuss.springdatabase.dao;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.odysseuss.springdatabase.entities.Officer;
@@ -12,6 +14,14 @@ import com.odysseuss.springdatabase.entities.Officer;
 @Repository
 public class JdbcOfficerDAO implements OfficerDAO {
 
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcOfficerDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
     @Override
     public long count() {
         // TODO Auto-generated method stub
@@ -20,7 +30,7 @@ public class JdbcOfficerDAO implements OfficerDAO {
 
     @Override
     public void delete(Officer officer) {
-        // TODO Auto-generated method stub
+        
         
     }
 
